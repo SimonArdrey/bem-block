f2a4c0ec3b40df178d925d4cdb51e876bc16ffc4
Simon Ardrey <simonardrey@gmail.com>
2017-05-03 23:29:23 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-/*
-  Created a simple BEM library because all the string concatenation was driving me crazy.
-  http://getbem.com/naming/
-  EXAMPLE:
-  Declare:
-  ```js
-  const css = bem('dashboard', 'fullscreen');
-  ```
-  Then in JSX
-  ```js
-  <div className={css()}>
-    <div className={css('widget', 'small')}></div>
-  </div>
-  ```
-  Will produce:
-  ```html
-  <div class="dashboard dashboard--fullscreen">
-    <div class="dashboard__widget dashboard__widget--small"></div>
-  </div>
-  ```
-*/
-
 export function block(blockName, ...blockModifiers) {
   function elementFn(elementName, ...elementModifiers) {
     let blockModifiersList = blockModifiers.length === 1 && typeof blockModifiers[0] !== 'string'
